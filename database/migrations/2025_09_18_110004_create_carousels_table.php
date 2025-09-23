6e0d49ff7f6b19bd360a2a980ccff3662712c0eb
Eduardo Sousa Cavalcante <131792350+EduardoSousaCavalcante@users.noreply.github.com>
2025-09-23 12:04:49 -0300

    Update 2025_09_18_110004_create_carousels_table.php

--- database/migrations/2025_09_18_110004_create_carousels_table.php
+++ database/migrations/2025_09_18_110004_create_carousels_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,6 +12,7 @@ return new class extends Migration
      */
     public function up()
     {
+        // Criar a tabela 'carousels'
         Schema::create('carousels', function (Blueprint $table) {
             $table->id();
             $table->string('titulo');
@@ -18,8 +20,32 @@ return new class extends Migration
             $table->string('imagem'); // nome da imagem
             $table->timestamps();
         });
-    }
 
+        // Inserir 3 carousels
+        DB::table('carousels')->insert([
+            [
+                'titulo' => 'Laranja e roxo',
+                'descricao' => 'gosto',
+                'imagem' => 'laranja_roxo.png',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'titulo' => 'Vermelho e Marrom',
+                'descricao' => 'Descrição do carrossel 2',
+                'imagem' => 'vermelho_marrom.png',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'titulo' => 'Azul e Rosa',
+                'descricao' => 'Descrição do carrossel 3',
+                'imagem' => 'azul_rosa.png',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+    }
 
     /**
      * Reverse the migrations.
